4ea900193a195a4986f66654656a4c63e0516a5d
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-04-07 21:23:12 +0200

    Task 1 ready

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+
+const markup =
+    galleryItems.map(({ preview, original, description }) =>
+    
+    `<li class="gallery__item")>
+        <a class="gallery__link" href= '${original}'>
+            <img
+                class="gallery__image"
+                src='${preview}'
+                data-source='${original}'
+                alt='${description}'
+            />
+        </a>
+    </li>`
+    ).join('');
+    
+
+galleryContainer.innerHTML += markup;
+
+
+galleryContainer.addEventListener('click', openModalWindow);
+
+function openModalWindow (event) {
+    event.preventDefault();
+
+    const instance =
+        basicLightbox.create
+            (`<img src = '${event.target.dataset.source}' 
+            width = '800' heigth = '600'>`);
+
+    instance.show();
+  
+};
+
